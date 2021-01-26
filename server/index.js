7db3f2459041be679d93875b9fae4a6469fa7d73
samjanderson <sam.andersonnn@live.ca>
2021-01-26 00:09:23 +0000

    clean up previous commit

--- server/index.js
+++ server/index.js
@@ -2,13 +2,13 @@
 
 // Basic express setup:
 
-const PORT          = 8080;
-const express       = require("express");
-const bodyParser    = require("body-parser");
-const app           = express();
+const PORT = 8080;
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static("public")); //anything from here can call it and display it on your HTML file
 
 // The in-memory database of tweets. It's a basic object with an array in it.
 const db = require("./lib/in-memory-db");
@@ -20,18 +20,25 @@ const db = require("./lib/in-memory-db");
 //
 // Because it exports a function that expects the `db` as a parameter, we can
 // require it and pass the `db` parameter immediately:
-const DataHelpers = require("./lib/data-helpers.js")(db);
+const DataHelpers = require("./lib/data-helpers.js")(db); //put the db in by default not changing it to any other value
 
 // Update the dates for the initial tweets (data-files/initial-tweets.json).
 require("./lib/date-adjust")();
 
 // The `tweets-routes` module works similarly: we pass it the `DataHelpers` object
 // so it can define routes that use it to interact with the data layer.
+//stores all the tweetsRoutes
 const tweetsRoutes = require("./routes/tweets")(DataHelpers);
 
 // Mount the tweets routes at the "/tweets" path prefix:
+//app.use is setting up settings for app so tweetsRoutes gets set up for app, tweets here is defining a route
+// /tweets adds a prefix to whatever route you have so youd have to go localhost/tweets/whatever route you had before
 app.use("/tweets", tweetsRoutes);
 
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
 });
+
+
+
+
