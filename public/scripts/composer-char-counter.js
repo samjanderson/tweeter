dfef096cfab8e519aa064f28f061ddbd8b3ccfd4
samjanderson <sam.andersonnn@live.ca>
2021-01-27 04:16:40 +0000

    refactor code to increase readability and remove unnecessary classes

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,10 +2,19 @@ $(document).ready(function () {
   $('#tweet-text').on('input', function (event) {
     let minusCounter = 140 - $(this).val().length;
     let changeCounter = $(this).siblings().children('.counter').html(minusCounter);
-    if (minusCounter < 0) {
-      changeCounter.addClass('minus-counter');
-    } else {
-      changeCounter.removeClass('minus-counter');
-    }
+    changeCounter.toggleClass('minus-counter', minusCounter < 0 );
+    
   });
 });
+
+// $(document).ready(function () {
+//   $('#tweet-text').on('input', function (event) {
+//     let minusCounter = 140 - $(this).val().length;
+//     let changeCounter = $(this).siblings().children('.counter').html(minusCounter);
+//     if (minusCounter < 0) {
+//       changeCounter.addClass('minus-counter');
+//     } else {
+//       changeCounter.removeClass('minus-counter');
+//     }
+//   });
+// });
