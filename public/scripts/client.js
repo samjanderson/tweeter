18df819f5de2cffa6266c36a8da0f7e52ffa88f4
samjanderson <sam.andersonnn@live.ca>
2021-01-27 16:24:10 +0000

    createTweetElement function complete HTML markup with template literals

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,3 +7,50 @@
 // document.addEventListener("dblclick", (event) => {
 //   console.log(event);
 // });
+
+
+const createTweetElement = function (tweet) {
+  const dateObj = new Date(tweet.created_at)
+  const utcString = dateObj.toUTCString()
+  const $tweet = $(`<article class="submitted-tweet"> 
+  <header class="tweet-header">
+    <div class="tweet-user-profile">
+    <img src='${tweet.user.avatars}'/>
+      <p class="username">${tweet.user.name}</p>
+    </div>
+    <span class="user-handle">${tweet.user.handle}</span>
+  </header>
+  <p class="composed-tweet-message">${tweet.content.text}</p>
+  <footer class="tweet-footer">
+    <p class="date-posted">Date posted</p>
+    <div class="composed-tweeter-icons">
+      <i class="fas fa-flag"></i>
+      <i class="fas fa-retweet"></i>
+      <i class="fas fa-heart"></i>
+    </div>
+  </footer>
+  </article>`);
+  return $tweet;
+};
+
+
+// Test / driver code (temporary). Eventually will get this from the server.
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+  "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+  "created_at": 1461116232227
+}
+
+const $tweet = createTweetElement(tweetData);
+const $tweet = $(`<article class="tweet">Hello world</article>`);
+
+// Test / driver code (temporary)
+console.log($tweet); // to see what it looks like
+$('.submitted-tweet').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+
