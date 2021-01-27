64ec5ee3c9eb07b356cf066ba6a4562946148eaa
samjanderson <sam.andersonnn@live.ca>
2021-01-27 22:39:55 +0000

    complete fetching tweets using Ajax Get

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -10,7 +10,7 @@
 
 $(document).ready(function () {
   submitHandler()
-  renderTweets(arrObjects);
+  loadTweets()
 });
 
 
@@ -23,22 +23,10 @@ const loadTweets = function () {
   $.ajax({
     method:"GET",
     url: '/tweets'
-  }).then(function (resolve, reject) {renderTweets(resolve)}) //something missing here ((resolve, reject) => {resolve('Success!');});
+  }).then(function (resolve, reject) {renderTweets(resolve)}) 
   .catch((error) => {console.log(error)})
 }
 
-//example from the other day
-// $(function() {
-//   const $button = $('#load-more-posts');
-//   $button.on('click', function () {
-//     console.log('Button clicked, performing ajax call...');
-//     $.ajax('more-posts.html', { method: 'GET' })
-//     .then(function (morePostsHtml) {
-//       console.log('Success: ', morePostsHtml);
-//       $button.replaceWith(morePostsHtml);
-//     });
-//   });
-// });
 
 //from lecture
 const submitHandler = function () {
@@ -51,7 +39,7 @@ const submitHandler = function () {
       method:"POST",
       data: text,
       url: '/tweets/'
-    }).then(console.log('success'))
+    }).then(console.log('success'))    //call loadTweets in here
     .catch((error) => {console.log(error)})
   });
 }
@@ -97,44 +85,44 @@ const renderTweets = function (tweets) {
 
 
 // Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
-  },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227 //1611773978583
-};
+// const tweetData = {
+//   "user": {
+//     "name": "Newton",
+//     "avatars": "https://i.imgur.com/73hZDYK.png",
+//     "handle": "@SirIsaac"
+//   },
+//   "content": {
+//     "text": "If I have seen further it is by standing on the shoulders of giants"
+//   },
+//   "created_at": 1461116232227 //1611773978583
+// };
 
-const $tweet = createTweetElement(tweetData);
+// const $tweet = createTweetElement(tweetData);
 
 
-const arrObjects = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1611594137419
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1611680537419
-  }
-];
+// const arrObjects = [
+//   {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": "https://i.imgur.com/73hZDYK.png",
+//       "handle": "@SirIsaac"
+//     },
+//     "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     "created_at": 1611594137419
+//   },
+//   {
+//     "user": {
+//       "name": "Descartes",
+//       "avatars": "https://i.imgur.com/nlhLi3I.png",
+//       "handle": "@rd"
+//     },
+//     "content": {
+//       "text": "Je pense , donc je suis"
+//     },
+//     "created_at": 1611680537419
+//   }
+// ];
 
 
